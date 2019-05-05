806e31945d7f22a96d0abe341ba5269720f0dcdc
M <49960850+maatt4code@users.noreply.github.com>
2019-05-06 00:31:50 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include "RotateSquareMatriix.hpp"
 
+#include <iostream>
+
+using namespace std;
+
 int main(int argc, char** argv)
 {
     // rotate square matrix
@@ -12,7 +16,23 @@ int main(int argc, char** argv)
             {{16, 17, 18, 19, 20}},
             {{21, 22, 23, 24, 25}}
         }};
+        cout << "#### Original matrix ##################################################################" << endl;
+        for( const auto& r : a) {
+            for( const auto& c : r ) {
+                cout << c << ",";
+            }
+            cout << endl;
+        }
+
         rotateSquareMatrix<ksize>(a);
+
+        cout << "#### Rotated matrix ##################################################################" << endl;
+        for( const auto& r : a) {
+            for( const auto& c : r ) {
+                cout << c << ",";
+            }
+            cout << endl;
+        }
     }
     return 0;
 }
